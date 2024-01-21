500bd5884ded2bd59d00fbc1f2769b3a2df0de8a
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-01-21 22:35:28 +0600

    duplicate unsorted

--- findingDuplicateSorted.cpp
+++ findingDuplicateSorted.cpp
@@ -52,12 +52,31 @@ int countingDuplicateHash(int A[], int length){
     }
 }
 
+int duplicateUnsorted(int A[], int length){
+    for(int i= 0; i<length -1; i++){
+        int count = 1;
+        if(A[i]!= -1){
+            for(int j = i+1; j<length; j++){
+                if(A[i]==A[j]){
+                    count++;
+                    A[j] = -1;
+                }
+            }
+        }
+        if(count> 1){
+            std::cout<<A[i]<<" "<<count<<std::endl;
+        }
+    }
+}
+
 int main()
 {
     int A[] = {3, 4, 4, 5, 6, 7, 9, 10, 10, 10, 15};
-
+    int B[] = {8,3,6,4,6,5,6,8,2,7};
     int length = sizeof(A) / sizeof(A[0]);
+     int length2 = sizeof(B)/sizeof(B[0]);
     // duplicateSorted(A, length);
     // countingDuplicatesSorted(A,length);
-    countingDuplicateHash(A,length);
+    // countingDuplicateHash(A,length);
+    duplicateUnsorted(B, length2);
 }
