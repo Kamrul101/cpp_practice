4c711aa9a3c216cdbea3dbe871c71e0dab4cfb23
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-01-23 23:40:02 +0600

    vector for each

--- stl_vector.cpp
+++ stl_vector.cpp
@@ -2,14 +2,26 @@
 using namespace std;
 int main(){
     int n;
-    vector<int> v;
     cin>>n;
+    vector<int> v(n);
+    
+    // int n;
+    // vector<int> v;
+    // cin>>n;
+    // v.resize(n);
+    // for(int i=0; i<n; i++){
+    //     int a;
+    //     cin>>a;
+    //     v.push_back(a);
+    // }
     for(int i=0; i<n; i++){
-        int a;
-        cin>>a;
-        v.push_back(a);
+        cin>>v[i];
     }
-    for(int i= 0; i<v.size(); i++){
-        cout<<v[i]<<" ";
+    // for(int i= 0; i<v.size(); i++){
+    //     cout<<v[i]<<" ";
+    // }
+    for(int u: v){
+        u += 2;
+        cout<<u<<" ";
     }
 }
