675fff80cf2d7653ab55670f1e40421ae20ed15b
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-01-23 23:14:27 +0600

    stl vector

--- pairofelementwithSumK.cpp
+++ pairofelementwithSumK.cpp
@@ -10,13 +10,36 @@ int pairOfSum(int A[], int length, int k){
     
  }
 }
+
+int pairOfSumHash(int A[], int length, int k){
+    int i;
+    int lower = A[0];
+    int higher = A[0];
+    for(i = 0; i<length; i++){
+        if(A[i]<lower){
+            lower = A[i];
+        }
+        if(A[i]>higher){
+            higher = A[i];
+        }
+    }
+    int size= higher + 1;
+    int H[size] = {0};
+    for(i=0;i<length; i++){
+        if((H[k-A[i]]) != 0){
+            std::cout<<A[i]<<" "<<k-A[i]<<std::endl;
+        } 
+        H[A[i]]++;
+    }
+}
 int main(){
     int k;
-    int B[] = {8,3,6,4,5,2,7};
+    int B[] = {8,3,6,4,5,2,7,15,-2,12};
     int length2 = sizeof(B)/sizeof(B[0]);
     std::cout<<"What would be the sum: ";
     std::cin>>k;
     std::cout<<std::endl;
-    pairOfSum(B,length2,k);
+    // pairOfSum(B,length2,k);
+    pairOfSumHash(B,length2,k);
 
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,18 @@
-// std::cout<<lower<<" "<<higher;
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y,z,count = 0;
+        cin>>z;
+    for(int i=1;i<=z ;i++){
+        if(z%i == 0){
+                count++;
+        }
+    }
+    if(count == 3){
+
+    return true;
+    }
+    else return false;
+    
+}
