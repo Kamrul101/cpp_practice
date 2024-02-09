502cb55d4ef75a6a3ffda4ed1c19bdd72f422a81
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-02-09 23:40:15 +0600

    search easy CF

---
+++ inSearchofEasyCF.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    optimize();
+    int n,a,s=0;
+    cin>>n;
+    bool con = false;
+    vector <int> v;
+    for(int i=0; i<n;i++){
+        cin>>a;
+        v.push_back(a);
+    }
+    for(int u: v){
+        s = s + u;
+    }
+    if(s>0){
+        cout<<"HARD\n";
+    }
+    else {
+        cout<<"EASY\n";
+    }
+
+    
+}
