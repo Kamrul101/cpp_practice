04cce7ffb4a8884f0e2d42d433e2865a58416a70
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-02-11 23:37:37 +0600

    CF

---
+++ gravityFlipCF.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    optimize();
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    for(auto u: v){
+        cout<<u<<" ";
+    }
+    
+
+    
+}
