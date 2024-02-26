124eb4a030ee2e9ad775e84d7e1c71ea9d6d2963
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-02-26 19:29:34 +0600

    vjudge

---
+++ vjudge/rectangle.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main() {
+    optimize();
+    
+    while(1){
+        int m ,n;
+        cin>>m>>n;
+        if(m==0 && n==0) break;
+        for(int i= 0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cout<<'#';
+            }
+            cout<<"\n";
+        }
+        cout<<"\n";
+        
+    }
+}
