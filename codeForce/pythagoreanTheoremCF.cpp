5e209ede95c8048a41b3c9a7c481d38487fe2835
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-02-29 21:46:50 +0600

    CF

--- codeForce/pythagoreanTheoremCF.cpp
+++ codeForce/pythagoreanTheoremCF.cpp
@@ -1,20 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-const int mx = 2e8 + 12;
-int h[mx];
+
+
 
 int main() {
     optimize();
     int n,count = 0;
     cin>>n;
-    for(int i = 1; i<=n;i++){
-        h[i*i] = 1;
-    }
+    
     for(int i= 1;i<=n;i++){
         for(int j=i; j<=n;j++){
-        int k = i*i + j*j;   
-        if(h[k]==1){
+        int k = sqrt(i*i + j*j);   
+        if(k<=n && (k*k == i*i+j*j)){
             count++;
         } 
         }
