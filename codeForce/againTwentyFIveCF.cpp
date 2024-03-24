d38e4d12ff1d179f6588e787f56a4bd6f302cb5f
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-03-24 22:48:29 +0600

    CF

---
+++ codeForce/againTwentyFIveCF.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+// const int mx = 2e6+123;
+// long long a[mx];
+
+int main() {
+    optimize();
+    long long a;
+    cin>>a;
+    //any thing power of 5 has last two digit 25 except power 0 and 1
+    cout<<25<<endl;
+    
+}
