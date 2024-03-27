af01a11922c9f16d83eaee72ef1ba2d671c5ef83
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-03-28 00:16:13 +0600

    CF

---
+++ codeForce/codeforcesCHeckingCF.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+// const int mx = 2e5+123;
+// int a[mx];
+
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        char c;
+        cin>>c;
+        if(c == 'c'||c =='o'||c =='d'||c =='e'||c =='f'||c =='o'||c =='r'||c =='c'||c =='e'||c =='s'){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+    
+}
