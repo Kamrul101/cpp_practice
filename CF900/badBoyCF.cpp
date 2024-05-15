8f43d4e49ebf626180b075c4d6869cbbb66938a0
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-05-15 23:10:16 +0600

    CF

---
+++ CF900/badBoyCF.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+typedef long long ll ;
+// const int mx = 2e5+123;
+// long long a[mx];
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,b,c;
+        cin>>n>>m>>b>>c;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+    }
+    
+    
+}
