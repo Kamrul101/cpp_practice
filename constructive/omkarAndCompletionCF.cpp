17ac42ed7f6650e4900c300231ba11e272c1ca78
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-05-24 00:04:31 +0600

    CF

---
+++ constructive/omkarAndCompletionCF.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+// const int mx = 2e5+123;
+// int a[mx];
+
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
