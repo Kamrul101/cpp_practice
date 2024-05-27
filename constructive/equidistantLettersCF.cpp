f1bb97458ce48c3741aa8472430b48ccfa36b4d4
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-05-27 23:54:19 +0600

    CF

---
+++ constructive/equidistantLettersCF.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+const int mx = 2e5+123;
+int a[mx];
+
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        sort(s.begin(),s.end());
+        cout<<s<<endl;
+
+    }
+}
