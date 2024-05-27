cd6b92b7e509daddd111e235ce3ee332dd8be639
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-05-27 22:39:09 +0600

    CF

---
+++ constructive/brickWallCF.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+// const int mx = 2e5+123;
+// int a[mx];
+
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        int n ,m;
+        cin>>n>>m;
+        
+        cout<<n*( m/2)<<endl;
+    }
+}
---
+++ constructive/preparingForTheContestCF.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+// const int mx = 2e5+123;
+// int a[mx];
+
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            v[i] = i+1;
+        }
+        reverse(v.begin()+k,v.end());
+        for(auto u:v){
+            cout<<u<<" ";
+        }
+        cout<<endl;
+
+    }
+}
