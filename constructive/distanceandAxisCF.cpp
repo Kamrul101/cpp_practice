8d425339eb5c0f5c79b60c6e23ac980b26c9111b
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-05-29 00:47:24 +0600

    CF

---
+++ constructive/distanceandAxisCF.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+// const int mx = 2e5+123;
+// int a[mx];
+
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int ans=0;
+        if(k>=n){
+            cout<<k-n<<endl;
+        }
+        
+        else{
+            cout<<(n-k)%2<<endl;
+        }
+    }
+}
