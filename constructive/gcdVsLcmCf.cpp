2b5ff0c3d6beb1de9165abd3ae2dd3dd0da26813
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-06-05 23:38:07 +0600

    CF

---
+++ constructive/gcdVsLcmCf.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+typedef long double ld ;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+// const int mx = 1e5+123;
+// int dp[mx]={0};
+
+
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+       ll n;
+       cin>>n;
+       cout<<n-3<<" "<<1<<" "<<1<<" "<<1<<endl;
+       
+    }
+    
+}
