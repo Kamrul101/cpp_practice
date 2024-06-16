5e93aa354cf155066b2f17e2f2f4babe89927242
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-06-16 21:49:33 +0600

    cf

---
+++ contest/aliceAndBooksCF.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+// const int mx = 2e5 + 123;
+
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        
+        ll ans = *max_element(v.begin(),v.end()-1)+v[n-1];
+        cout<<ans<<endl;
+
+    }
+}
