a8058f202c31d8a4b6e5171caa2b3121740e3741
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-06-29 12:38:29 +0600

    CF

---
+++ binarySearch/kthNotDivisibleBynCF.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+const int mx = 2e5 + 123;
+// ll a[mx],b[mx];
+
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        ll n,k;
+        cin>>n>>k;
+        ll l=1, h= 1e18;
+        ll ans=0;
+        while(l<=h){
+            ll mid = (l+h)/2;
+            ll x  = mid-(mid/n);
+            
+            if(x>=k) {
+                ans = mid;
+                h = mid -1;
+
+            }
+            else l = mid +1;
+        }
+        cout<<ans<<endl;
+    }
+    
+}
