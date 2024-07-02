b3eb91619551676ccae2485385e3d92da904490c
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-07-02 15:24:24 +0600

    CF

--- CF800/basilsGardenCF.cpp
+++ CF800/basilsGardenCF.cpp
@@ -18,14 +18,11 @@ int main()
         int n;
         cin>>n;
         for(int i=0;i<n;i++) cin>>a[i];
-        ll ans =0,mx=0;
-        for(int i=1;i<n;i++){
-            ll  diff = max(a[i-1]-a[i],0LL);
-            mx =  max(mx,diff);
-            ans+=diff;
-            a[i]+=diff;
+        ll ans =0;
+        for(int i=n-1;i>=0;i--){
+            ans=max(ans,a[i]+i);
         }
-        cout<<ans+mx<<endl;
+        cout<<ans<<endl;
        
     }
 }
--- CF800/uploadMoreRamCF.cpp
+++ CF800/uploadMoreRamCF.cpp
@@ -3,6 +3,7 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
 #define endl "\n"
 #define MOD 1000000007
 #define mem(a,b) memset(a, b, sizeof(a) )
