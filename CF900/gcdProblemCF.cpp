f823a14b7bb7dca9d04f93a61201429ff4a17adf
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2024-08-12 23:52:12 +0600

    CF

---
+++ CF900/gcdProblemCF.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+const int mx =2e5+123;
+int a[mx];
+int main() {
+    optimize();
+    
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    int a,b;
+    for(int i=2;i<n;i++){
+        b = (n-i-1);
+        if(__gcd(i,b)==1){
+            a=i;
+            break;
+        }
+    }
+    cout<<a<<" "<<b<<" "<<1<<endl;
+    } 
+}
