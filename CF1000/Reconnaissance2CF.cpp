cd774558eddb7a273837277ad2417159e9f017f9
kamrul101 <ridoyhasan87@gmail.com>
2024-09-09 16:24:04 +0600

    CF

--- CF1000/Reconnaissance2CF.cpp
+++ CF1000/Reconnaissance2CF.cpp
@@ -14,9 +14,22 @@ int main() {
     int n;
     cin>>n;
 
-    int mnI,currI;
+    int mnI,currI,mn=10001;
     for(int i=0;i<n;i++){
-
+        cin>>a[i];
     }
+
+    for(int i=0;i<n-1;i++){
+            if(abs(a[i]-a[i+1])<mn){
+                mn=abs(a[i]-a[i+1]);
+                currI=i;
+                mnI=i+1;   
+            }
+    }
+    if(abs(a[0]-a[n-1])<mn){
+        currI=0;
+        mnI=n-1;
+    }
+    cout<<currI+1<<" "<<mnI+1<<endl;
     
 }
