b7459b8e2c8db937a7e9f8e586aa231849cda7c4
kamrul101 <ridoyhasan87@gmail.com>
2024-09-09 16:00:25 +0600

    CF

---
+++ CF1000/Reconnaissance2CF.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+const int mx =2e5+123;
+int a[mx];
+int main() {
+    optimize();
+    int n;
+    cin>>n;
+
+    int mnI,currI;
+    for(int i=0;i<n;i++){
+
+    }
+    
+}
