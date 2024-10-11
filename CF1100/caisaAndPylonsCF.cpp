ed475ca30976a2e7d0ca6f78cfb90e2fec6eddb9
kamrul101 <ridoyhasan87@gmail.com>
2024-10-11 23:31:13 +0600

    CF

---
+++ CF1100/caisaAndPylonsCF.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+// const int mx =2e5+123;
+// int a[mx];
+int main() {
+    optimize();
+    int n,ans=INT_MIN;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        ans = max(x,ans);
+    }
+    cout<<ans<<endl;
+    
+}
