3685110201586e4719632b34846e3ff8b84bdb65
kamrul101 <ridoyhasan87@gmail.com>
2024-11-21 20:11:01 +0600

    CF

---
+++ CF1100/tempCodeRunnerFile.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+// const int mx =2e5+123;
+// int a[mx];
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n),b(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        for(int i=0;i<n;i++) cin>>b[i];
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        ll ans = 1;
+        for(int i=0,j=0;i<n;i++){
+            while(j<n && a[i]>b[j]){
+                j++;
+            }
+            ans *= j-i;
+        }
+        cout<<ans<<endl;
+        
+    }
+    
+}
