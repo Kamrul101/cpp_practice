45eadd2091fb65863b4820d9d4c4d6852c8ffab3
kamrul101 <ridoyhasan87@gmail.com>
2024-12-16 14:44:07 +0600

    CF

--- contest/993 div 4/5.cpp
+++ contest/993 div 4/5.cpp
@@ -14,7 +14,39 @@ int main() {
     int t;
     cin>>t;
     while(t--){
-        
+        ll k,l1,r1,l2,r2;
+        cin>>k>>l1>>r1>>l2>>r2;
+        ll ans = 1,sum=0;
+        for(int i=1;i<=32;i++){
+            ll l=l1,r=r1,left = 0,right=0;
+            while(l<=r){
+                ll mid = (l+r)/2;
+                ll temp = ans*mid;
+                if(temp>=l2){
+                    left = mid;
+                    r = mid -1;
+                }
+                else l = mid+1;
+            }
+            l = l1, r = r1;
+            while(l<=r){
+                ll mid = (l+r)/2;
+                ll temp = ans*mid;
+                if(temp<=r2){
+                    right = mid;
+                    l = mid+1;
+                }
+                else r = mid-1;
+            }
+            if(left!=0 && right!=0){
+                sum+=(right-left+1);
+            }
+            if(ans*k*1LL>r2){
+                break;
+            }
+            ans*=k*1LL;
+        }
+        cout<<sum<<endl;
         
     }
     
