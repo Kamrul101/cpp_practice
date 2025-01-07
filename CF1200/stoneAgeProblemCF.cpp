c87994d451e4a12e7ab99be1fcf6a9b911afbffc
kamrul101 <ridoyhasan87@gmail.com>
2025-01-07 23:42:48 +0600

    CF

--- CF1200/stoneAgeProblemCF.cpp
+++ CF1200/stoneAgeProblemCF.cpp
@@ -7,15 +7,42 @@ typedef long double ld;
 #define endl "\n"
 #define MOD 1000000007
 #define mem(a,b) memset(a, b, sizeof(a) )
-// const int mx =2e5+123;
-// int a[mx];
+const int mx =2e5+123;
+ll v[mx],t[mx];
 int main() {
     optimize();
-    int t;
-    cin>>t;
-    while(t--){
-        
-        
+    int n,q;
+    cin>>n>>q;
+    ll sum=0;
+    for(ll i=0;i<n;i++){
+        cin>>v[i];
+        sum+=v[i];
+        t[i]=-1;
     }
+    ll x,a,b,c, lastSecondQuery=-2,lastSecondQueryval= 0;
+    for(int i=0;i<q;i++){
+        cin>>x;
+        if(x==1){
+            cin>>a>>b;
+            a--;
+            if(lastSecondQuery>t[a]){
+                sum=sum-lastSecondQueryval+b;
+            }
+            else{
+                sum = sum-v[a]+b;
+                
+            }
+            t[a] = i;
+            v[a] = b;
+        }
+        else{
+            cin>>c;
+            sum=(n*c);
+            lastSecondQuery=i;
+            lastSecondQueryval = c;
+        }
+        cout<<sum<<endl;
+    }
+
     
 }
