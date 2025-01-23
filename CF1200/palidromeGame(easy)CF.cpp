6e72efc37cc586328533bb22147f30a7abf41147
kamrul101 <ridoyhasan87@gmail.com>
2025-01-23 22:28:20 +0600

    CCF

---
+++ CF1200/palidromeGame(easy)CF.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+// const int mx =2e5+123;
+// int a[mx];
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0') cnt++;
+        }
+        if(cnt==0) cout<<"DRAW"<<endl;
+        else if(cnt==1) cout<<"BOB"<<endl;
+        else if(cnt%2) cout<<"ALICE"<<endl;
+        else cout<<"BOB"<<endl;
+        
+        
+    }
+    
+}
