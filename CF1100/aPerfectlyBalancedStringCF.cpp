ee864dd67623112b73e63618c9e8eb438bdbfe67
kamrul101 <ridoyhasan87@gmail.com>
2025-02-15 22:09:57 +0600

    CF

---
+++ CF1100/aPerfectlyBalancedStringCF.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+// const int mx =2e5+123;
+// int a[mx];
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        set<char> s1;
+        int i;
+        for(i=0;i<s.size();i++){
+            if(s1.find(s[i])==s1.end()){
+                s1.insert(s[i]);
+            }
+            else break;
+        }
+        bool check = true;
+        for(int k = i;k<s.size();k++){
+            if(s[k]!=s[k-i]){
+                check = false;
+                cout<<"NO"<<endl;
+                break;
+            }
+        }
+        if(check) cout<<"YES"<<endl;
+        
+    }
+    
+}
