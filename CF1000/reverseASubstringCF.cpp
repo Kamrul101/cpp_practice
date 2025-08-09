ddcddef8825cfdcf05e112982a702a7ffe05fdcf
kamrul101 <ridoyhasan87@gmail.com>
2025-08-09 23:17:19 +0600

    CF

---
+++ CF1000/reverseASubstringCF.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+// const int mx =2e5+123;
+// int a[mx];
+int main() {
+    optimize();
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    string temp = s;
+    sort(temp.begin(),temp.end());
+    if(s==temp){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+        bool check = false;
+        for(int i=0;i<n;i++){
+            if(s[i]!=temp[i]){
+                for(int j=i+1;j<n;j++){
+                    if(s[j]==temp[i]){
+                        cout<<i+1<<" "<<j+1<<endl;
+                        check=true;
+                        break;
+                    }
+                }
+            }
+            if(check) break;
+        }
+    }
+    
+}
