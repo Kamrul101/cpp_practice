f345122cb37810e258aef09f425f05fb50b704f6
kamrul101 <ridoyhasan87@gmail.com>
2025-09-12 23:11:04 +0600

    CF

---
+++ contest/1049 div 2/1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+// const int mx =2e5+123;
+// int a[mx];
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s,t;
+        cin>>s;
+        t = s;
+        sort(t.begin(),t.end());
+        int cnt = 0;
+        for(int i=0;i<n;i++) if(s[i]!=t[i]) cnt++;
+        cout<<cnt/2<<endl;
+        
+    }
+    
+}
