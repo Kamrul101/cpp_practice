c5509e7d04bc7c356b5fb344dad8d02fd336c89d
kamrul101 <ridoyhasan87@gmail.com>
2025-09-26 20:50:46 +0600

    CF

---
+++ Interactive/isItrated.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl "\n"
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+// const int mx =2e5+123;
+// int a[mx];
+int main() {
+    optimize();
+    string s;
+    while(cin>>s){
+        cout<<"NO"<<endl;
+        cout<<flush;
+    }
+    return 0;
+    
+}
